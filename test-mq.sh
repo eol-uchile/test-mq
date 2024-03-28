0f025fec780b681cc09ac57b564a0e61fb194a0f
matiasua <matcardenas@uchile.cl>
2024-03-28 12:10:09 -0300

    se modifico el script

--- test-mq.sh
+++ test-mq.sh
@@ -12,7 +12,7 @@ archivo="$1"
 if grep -q "ya valiste" "$archivo"; then
   echo "Se encontró la palabra 'ya valiste'. FALLO."
   exit 1
-elif grep -q "ya coronamos" "$archivo"; then
+elif grep -q "ya wey" "$archivo"; then
   echo "Se encontró la palabra 'ya wey'. Éxito."
   exit 0
 else
